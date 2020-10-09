e1cd374b193b111b97cc8d09afdd8c165339fe65
mathias73 <mat.micheli99@gmail.com>
2020-10-09 17:48:36 +0200

    add movie player page and vertical list film

---
+++ components/movie/verticalListFilm.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h3 class="is-size-3 title">Films similaires : </h3>
+      <div class="column" v-for="video in videos">
+        <p class="has-text-centered">{{ video.author }}</p>
+        <a :href="video.download_url">
+          <img :src="video.download_url" alt="Miniature d'un chapitre vidéo">
+        </a>
+      </div>
+    </div>
+</template>
+<script>
+
+export default {
+  name: "verticallistfilm",
+  data() {
+    return {
+      videos: {}
+    }
+  },
+  methods: {
+    async getVideo() {
+      await this.$axios.$get('https://picsum.photos/v2/list?limit=3')
+        .then(response => (this.videos = response))
+        .catch(error => console.log(error));
+    }
+  },
+  mounted() {
+    this.getVideo();
+  }
+}
+</script>
+
+<style scoped>
+.test {
+  border: deeppink solid 1px;
+}
+</style>
---
+++ pages/movie.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container mt-4">
+    <div class="columns">
+      <div class="column is-three-quarters">
+        <video controls >
+          <source src="http://techslides.com/demos/sample-videos/small.mp4" type="video/mp4">
+        </video>
+        <h3 class="is-size-3 mb-4">Un film cool</h3>
+        <p class="mb-3">16:07 min | 301 vues | 13 octobre 2020</p>
+        <p class="mb-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum, eligendi ullam. Autem debitis deleniti dolorum impedit iure mollitia nisi quae ratione recusandae sit? Aliquid autem facere ipsa iste laboriosam minima!</p>
+        <button class="button is-link is-light">Partager !</button>
+      </div>
+      <div class="column">
+        <verticallistfilm/>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+
+import verticallistfilm from "../components/movie/verticalListFilm";
+
+export default {
+  components: {verticallistfilm},
+  name: "movie",
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+.test {
+  border: deeppink solid 1px;
+}
+</style>
